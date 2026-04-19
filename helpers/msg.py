2d7b888e9c460afba83bec36ba6fd639cc10749e
prono69 <faltu9851@gmail.com>
2026-04-19 20:26:33 +0530

    update msg.py

--- helpers/msg.py
+++ helpers/msg.py
@@ -52,7 +52,7 @@ def get_file_name(message_id: int, chat_message) -> str:
     elif chat_message.video_note:
         return f"{message_id}.mp4"
     elif chat_message.animation:
-        return chat_message.animation.file_name or f"{message_id}.gif"
+        return chat_message.animation.file_name or f"{message_id}.mp4"
     elif chat_message.sticker:
         if chat_message.sticker.is_animated:
             return f"{message_id}.tgs"
